f2f78dfa5ca234fa711e19af92060045d2357ee3
Aldukard <juarezgaonad@gmail.com>
2023-03-02 18:25:29 -0600

    Segundo intendo subiendo mi ejercicio

--- App.js
+++ App.js
@@ -1,11 +1,60 @@
+import React, {useState} from "react"
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  /**
+  const getDatosALumno = (nombre,carrera, especialidad) => {
+    return nombre +" "+ carrera + " " + especialidad;
+  }
+
+  //const [activo, setActivo] = useState(true)
+  //const [materia, setMateria] = useState("Inscribe una materia")
+
+  /**const Materias=(props) => {
+    return(
+      <view>
+        <Text>{props.nombre}</Text>
+      </view>
+    )
+  }*/
+
+  const [materia1, setMateria1] = useState("Inscribe una materia")
+  const [materia2, setMateria2] = useState("Inscribe una materia")
+  const [materia3, setMateria3] = useState("Inscribe una materia")
+  const [activo, setMostrar] = useState(true)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+      style={styles.cajaTexto}
+      defaultValue={materia1}    
+      onChangeText={(newText) => setMateria1(newText)}
+      />
+      <TextInput
+      style={styles.cajaTexto}
+      defaultValue={materia2}
+      onChangeText={(newText) => setMateria2(newText)}
+      />
+      <TextInput
+      style={styles.cajaTexto}
+      defaultValue={materia3}
+      onChangeText={(newText) => setMateria3(newText)}
+      />
+
+      <Button
+      onPress={()=>{
+        setMostrar(!activo)
+      }}
+      
+      title = {"Imprimir"}
+      />
+      <View >
+      {activo ? "Sin materias": <><Text>Materias Inscritas</Text><Text>{materia1}</Text><Text>{materia2}</Text><Text>{materia3}</Text></>}
+
+      </View>
+
     </View>
   );
 }
@@ -15,6 +64,34 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center',  
   },
 });
+
+/**
+<Text>Hola {getDatosALumno("Daniel Alejandro","ISC","FullStack")}</Text>
+      <TextInput 
+        style={styles.cajaTexto}
+        defaultValue="Proporciona el nombre"
+      />
+      <Text>MATERIAS</Text>
+      <Materias nombre="Aplicacionez hibridas"/>
+      <Materias nombre="I teligencia artificial"/>
+      <Materias nombre="Administracion de redes"/>
+      <Materias nombre="Arquitectura de Servicios"/>
+
+      <TextInput
+      style={styles.cajaTexto}
+      onChange={(newText) => setMateria(newText)}
+      defaultValue={materia}
+      />
+
+      <Button
+        onPress={() => {
+          setActivo(false);
+        }}
+        disabled={activo}
+        title={activo ? "Click...":"No activo"}
+      /> 
+      <StatusBar style="auto" />
+  */
