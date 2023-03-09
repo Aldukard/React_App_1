ae4e8357ada67d897a66d126c731281ab59d92de
Aldukard <juarezgaonad@gmail.com>
2023-03-08 22:38:01 -0600

    Actividad 6

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react"
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View, FlatList, StatusBar } from 'react-native';
 
 export default function App() {
 
@@ -17,13 +17,77 @@ export default function App() {
     )
   }*/
 
+    const [datos, setDatos] = useState([
+      { key: "Materia 1", color: false },
+      { key: "Materia 2", color: false },
+      { key: "Materia 3", color: false },
+      { key: "Materia 4", color: false },
+      { key: "Materia 5", color: false },
+      { key: "Materia 6", color: false },
+      { key: "Materia 7", color: false },
+      { key: "Materia 8", color: false },
+      { key: "Materia 9", color: false },
+      { key: "Materia 10", color: false },
+      { key: "Materia 11", color: false },
+    ]);
+
+    
+
+    let materiaBuscada = "";
+    const Materias = (props) => {
+      return (
+        <View>
+          <Text style={props.color ? styles.materiasActiva : styles.textoNorm}>
+            {props.nombre}
+          </Text>
+        </View>
+      );
+    };
+
+  /*
   const [materia1, setMateria1] = useState("Inscribe una materia")
   const [materia2, setMateria2] = useState("Inscribe una materia")
   const [materia3, setMateria3] = useState("Inscribe una materia")
   const [activo, setMostrar] = useState(true)
+  */
 
   return (
     <View style={styles.container}>
+      <Text style={styles.topbar}>{"Materias"}</Text>
+
+      <FlatList
+        data={datos}
+        renderItem={({ item }) => (
+          <Materias nombre={item.key} color={item.color} />
+        )}
+      />
+
+      <TextInput
+        style={styles.disenhoCaja}
+        onChangeText={(newText) => (materiaBuscada = newText)}
+        placeholder={"Buscar una materia"}
+      />
+      <Button
+        onPress={() => {
+          for (let index = 0; index < datos.length; index++) {
+            if (datos[index].key === materiaBuscada) {
+              console.log(datos[index].key);
+              const datosnuevos = [...datos];
+              datosnuevos[index].color = true;
+              setDatos(datosnuevos);
+            }
+          }
+        }}
+        title={"Revisar materia"}
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+
+  /*
+  return (
+    <View style={styles.container}
+    >
 
       <Text style={{
         fontSize:25,
@@ -40,7 +104,7 @@ export default function App() {
       <TextInput
       style={styles.cajaTexto}
       defaultValue={materia2}
-      onChangeText={(newText) => setMateria2(newText)}
+      onChangeText={(newText) => setMateria2(newText)}  
       />
       <TextInput
       style={styles.cajaTexto}
@@ -63,18 +127,15 @@ export default function App() {
       </View>
 
     </View>
-  );
+  );*/
 }
-
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-    margin:10  
+    justifyContent: 'center'
   },
   cajaTexto:{
     height:40,
@@ -82,10 +143,6 @@ const styles = StyleSheet.create({
     borderWidth:2,
     minWidth:200,
     margin:10
-  },
-  titleText: {
-    fontSize: 20,
-    fontWeight: 'bold',
   },
   button: {
     fontSize: 20,
@@ -95,6 +152,23 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color:"green",
     margin:10  
+  },
+  topbar: {
+    fontSize: 30,
+    fontWeight: "bold",
+    margin: 20,
+    width: 500,
+    color:"#23E4ED",
+    backgroundColor: "#BB1880",
+    textAlign: "center",
+  },
+  materiasActiva: {
+    fontSize: 30,
+    textAlign: "center",
+    margin: 5,
+    fontStyle: "italic",
+    color: "pink",
+    backgroundColor: "blue"
   }
 });
 
@@ -124,4 +198,4 @@ const styles = StyleSheet.create({
         title={activo ? "Click...":"No activo"}
       /> 
       <StatusBar style="auto" />
-  */
+*/
